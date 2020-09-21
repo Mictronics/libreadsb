0c71cff09f5ab23ee2d7e9b94960869633ae3fe5
Mictronics <github@mictronics.de>
2020-09-21 20:16:04 +0200

    Inline functions required by track.

--- include/mode_ac.h
+++ include/mode_ac.h
@@ -8,6 +8,18 @@ extern "C"
 
 #include "readsb_def.h"
 
+    /* Convert from a (hex) mode A value to a 0-4095 index */
+    static inline unsigned mode_a_to_index(unsigned mode_a)
+    {
+        return (mode_a & 0x0007) | ((mode_a & 0x0070) >> 1) | ((mode_a & 0x0700) >> 2) | ((mode_a & 0x7000) >> 3);
+    }
+
+    /* Convert from a 0-4095 index to a (hex) mode A value */
+    static inline unsigned index_to_mode_a(unsigned index)
+    {
+        return (index & 0007) | ((index & 0070) << 1) | ((index & 0700) << 2) | ((index & 07000) << 3);
+    }
+
     void decode_mode_a_message(modes_message_t *mm, int ModeA);
     void mode_ac_init();
     int mode_a_to_mode_c(unsigned int modeA);
--- src/mode_ac.c
+++ src/mode_ac.c
@@ -7,18 +7,6 @@
 static int mode_a_to_c_lut[4096];
 static unsigned mode_c_to_a_lut[4096];
 
-/* Convert from a (hex) mode A value to a 0-4095 index */
-static inline unsigned mode_a_to_index(unsigned mode_a)
-{
-    return (mode_a & 0x0007) | ((mode_a & 0x0070) >> 1) | ((mode_a & 0x0700) >> 2) | ((mode_a & 0x7000) >> 3);
-}
-
-/* Convert from a 0-4095 index to a (hex) mode A value */
-static inline unsigned index_to_mode_a(unsigned index)
-{
-    return (index & 0007) | ((index & 0070) << 1) | ((index & 0700) << 2) | ((index & 07000) << 3);
-}
-
 static int internal_mode_a_to_mode_c(unsigned int mode_a)
 {
     unsigned int FiveHundreds = 0;
