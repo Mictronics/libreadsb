96031bd3f2e0fbf3d447d50eee0960fb009f9e37
Mictronics <github@mictronics.de>
2020-09-22 18:19:55 +0200

    Add SDR configuration.

--- include/readsb.h
+++ include/readsb.h
@@ -10,7 +10,6 @@ extern "C"
     enum sdr_type
     {
         SDR_NONE = 0,
-        SDR_IFILE,
         SDR_RTLSDR,
         SDR_BLADERF,
         SDR_MICROBLADERF,
@@ -19,34 +18,69 @@ extern "C"
         SDR_GNS
     };
 
+    /* Library configuration */
     typedef struct
     {
-        enum sdr_type sdr_type; // where are we getting data from?
+        unsigned freq;              // Receiver frequency we are listen on
+        unsigned dc_filter;         // Should we apply a DC filter?
+        unsigned nfix_crc;          // Number of crc bit error(s) to correct
+        unsigned check_crc;         // Only display messages with good CRC
+        unsigned mode_ac;           // Enable decoding of SSR Modes A & C
+        unsigned metric;            // Use metric units
+        unsigned use_gnss;          // Use GNSS altitudes with H suffix ("HAE", though it isn't always) when available
+        unsigned max_range;         // Maximum decoding range in meters
+        unsigned stats_polar_range; // Collect/show a range histogram?
+        int altitude;               // Receiver altitude.
+        double latitude;            // Receiver location latitude.
+        double longitude;           // Receiver location longitude.
+    } readsb_config_t;
+
+    /* RTL-SDR device configuration */
+    typedef struct
+    {
         char *dev_name;
         int gain;
         int enable_agc;
-        int freq;
         int ppm_error;
         int biastee;
-        int dc_filter;         // should we apply a DC filter?
-        int nfix_crc;          // Number of crc bit error(s) to correct
-        int check_crc;         // Only display messages with good CRC
-        int mode_ac;           // Enable decoding of SSR Modes A & C
-        int mode_ac_auto;      // allow toggling of A/C by Beast commands
-        int metric;            // Use metric units
-        int use_gnss;          // Use GNSS altitudes with H suffix ("HAE", though it isn't always) when available
-        int max_range;         // Maximum decoding range in meters
-        int stats_polar_range; // Collect/show a range histogram?
-        int altitude;          // Receiver altitude.
-        double latitude;       // Receiver location latitude.
-        double longitude;      // Receiver location longitude.
-    } readsb_config_t;
+    } readsb_rtlsdr_config_t;
+
+    /* BladeRF and uBladeRF device configuration */
+    typedef struct
+    {
+        char *dev_name;
+        char *fpga_path;
+        unsigned decimation;
+        unsigned bandwidth;
+    } readsb_bladerf_config_t;
+
+    /* ADLAM-Pluto device configuration */
+    typedef struct
+    {
+        char *uri;
+        char *network;
+        int gain;
+    } readsb_plutosdr_config_t;
+
+    /* Mode-S Beast and GNS Hulc device configuration */
+    typedef struct
+    {
+        char *dev_name;
+        int filter_df045;
+        int filter_df1117;
+        int mode_ac;
+        int mlat_timestamp;
+        int fec;
+        int crc;
+    } readsb_beastgns_config_t;
 
     READSB_API void say_hello();
 
-    READSB_API void readsb_init(readsb_config_t *config);
-    READSB_API void readsb_open();
-    READSB_API void readsb_close();
+    READSB_API int readsb_init(readsb_config_t *lib_config);
+    READSB_API int readsb_open(enum sdr_type sdr_type, void *sdr_config);
+    READSB_API int readsb_close();
+    READSB_API unsigned readsb_get_aircraft_count();
+    READSB_API void *readsb_get_aircraft_by_address(unsigned addr);
 
 #ifdef __cplusplus
 }
